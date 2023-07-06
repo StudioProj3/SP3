aa41cfcfa0f2a915d3f2fa0d9a70d0d3dad25b60
Christian <97773844+esuriru@users.noreply.github.com>
2023-07-06 22:40:31 +0800

    Refactored and added ChangeState function

--- Assets/Scripts/AI/FiniteStateMachine.cs
+++ Assets/Scripts/AI/FiniteStateMachine.cs
@@ -43,11 +43,13 @@ public class FiniteStateMachine<T>
 
     public void SetState(params Type[] stateTypes)
     {
-        Debug.Assert(stateTypes.Where(t => !t.IsSubclassOf(typeof(State<T>))).Count() > 0, "Invalid Type passed into SetState function.");
+        Debug.Assert(stateTypes.Where(t => !t.IsSubclassOf(typeof(State<T>)))
+            .Count() > 0, "Invalid Type passed into SetState function.");
 
         if (_currentStates.IsNullOrEmpty())
         {
             _currentStates.ForEach(s => s.Exit());
+            _currentStates.Clear();
         }
 
         foreach (Type stateType in stateTypes)
@@ -64,14 +66,59 @@ public class FiniteStateMachine<T>
                 }
                 else
                 {
-                    Debug.LogWarningFormat("Type passed into SetState was already passed in. Type name: '{0}'", stateType.Name);
+                    Debug.LogWarningFormat(
+                        "Type passed was already passed in. Type name: '{0}'",
+                        stateType.Name);
                 }
             }
             else
             {
-                Debug.LogWarningFormat("Type passed into SetState was not added first. Type name: '{0}'", stateType.Name);
+                Debug.LogWarningFormat(
+                    "Type passed was not added first. Type name: '{0}'",
+                    stateType.Name);
             }
         }
+    }
+
+    public void ChangeState<TF, TT>() where TF : State<T> where TT : State<T>
+    {
+        // TODO (Chris): Review this. Not completely sure if this works.
+
+        // FIXME (Chris): We might want to change the list into a dictionary so we
+        // can have a faster lookup.
+
+        // NOTE (Chris): We want to find the state that is easier to look up
+        // first. This ties up with the FIXME above this.
+        if (_states.TryGetValue(typeof(TT), out State<T> toState))
+        {
+            return;
+        }
+
+        var fromStatePairEnumerable =
+            _currentStates.Select((state, index) =>
+                new { state, index = index + 1 })
+                .Where(pair => pair.state.GetType() == typeof(TF));
+
+        // This will return -1 if there is no state found that matches.
+        var index = fromStatePairEnumerable.Select(pair => pair.index)
+                    .FirstOrDefault() - 1;
+        
+        if (index == -1)
+        {
+            return;
+        }
+
+        State<T> fromState = fromStatePairEnumerable
+            .Select(pair => pair.state).FirstOrDefault();
+        
+        // Now that we have both states, call the enter and exit functions.
+        fromState.Exit();
+
+        // Remove the current state and add the new one.
+        _currentStates.RemoveAt(index);
+        _currentStates.Add(toState);
+
+        toState.Enter();
     }
 
     #endregion
