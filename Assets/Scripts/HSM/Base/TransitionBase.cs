6662db6c0264b6039768896d68ab6a3ad6d9069e
Rageking8 <tomleetyt@gmail.com>
2023-08-14 00:31:37 +0800

    Add HSM base classes together with some interfaces.
    New classes include `SMChild`, `StateBase`, `StateMessageMethod`, `TransitionBase` and `IStateMachine`.

---
+++ Assets/Scripts/HSM/Base/TransitionBase.cs
@@ -0,0 +1,41 @@
+using UnityEngine.Assertions;
+
+public abstract class TransitionBase<TStateID> :
+    SMChild<TStateID>
+{
+    public TStateID FromStateID { get; protected set; }
+    public TStateID ToStateID { get; protected set; }
+
+    public abstract bool Conditions();
+
+    // To allow the helper class at the bottom
+    // to perform their own assertions and initialization
+    protected TransitionBase()
+    {
+
+    }
+
+    protected TransitionBase(TStateID fromStateID, TStateID toStateID)
+    {
+        Assert.IsTrue(fromStateID != null && toStateID != null);
+
+        FromStateID = fromStateID;
+        ToStateID = toStateID;
+    }
+}
+
+// A helper abstract class `TransitionBase` with the
+// generic argument defaulted to a common type
+public abstract class TransitionBase :
+    TransitionBase<string>
+{
+    protected TransitionBase(string fromStateID, string toStateID)
+    {
+        // Ensure that the strings passed in is neither null nor empty
+        Assert.IsTrue(!string.IsNullOrEmpty(fromStateID) &&
+            !string.IsNullOrEmpty(toStateID));
+
+        FromStateID = fromStateID;
+        ToStateID = toStateID;
+    }
+}
