f3295d58cebf31cd91e28746257aa6b613a04c35
Christian <97773844+esuriru@users.noreply.github.com>
2023-08-13 13:26:50 +0800

    Formatted `IsNullOrEmpty` to a one-liner.

--- Assets/Scripts/Utils/Helpers/ExtensionMethods.cs
+++ Assets/Scripts/Utils/Helpers/ExtensionMethods.cs
@@ -4,21 +4,12 @@ using System.Linq;
 public static class ExtensionMethods
 {
     #region Public
+
     /// Determines whether the collection is null or contains no elements.
     public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
     {
-        if (enumerable == null)
-        {
-            return true;
-        }
-        /* If this is a list, use the Count property for efficiency. 
-         * The Count property is O(1) while IEnumerable.Count() is O(N). */
-        var collection = enumerable as ICollection<T>;
-        if (collection != null)
-        {
-            return collection.Count < 1;
-        }
-        return !enumerable.Any(); 
+        return enumerable == null || !enumerable.Any(); 
     }
+
     #endregion
 }
