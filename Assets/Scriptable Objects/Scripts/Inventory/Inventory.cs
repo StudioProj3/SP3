57b43fa131493bea1392fdc35f6b95cbcd80ef75
Rageking8 <tomleetyt@gmail.com>
2023-08-14 12:15:23 +0800

    Add `NumSlots` and `MaxPerSlot` properties to `Inventory`.

--- Assets/Scriptable Objects/Scripts/Inventory/Inventory.cs
+++ Assets/Scriptable Objects/Scripts/Inventory/Inventory.cs
@@ -5,5 +5,23 @@ using UnityEngine;
 public class Inventory :
     ScriptableObject
 {
+    public uint NumSlots
+    {
+        get => _numSlots;
+        private set => _numSlots = value;
+    }
 
+    public uint MaxPerSlot
+    {
+        get => _maxPerSlot;
+        private set => _maxPerSlot = value;
+    }
+
+    [SerializeField]
+    [Tooltip("Number of slots in the inventory")]
+    private uint _numSlots;
+
+    [SerializeField]
+    [Tooltip("Max number of items in 1 slot")]
+    private uint _maxPerSlot;
 }
