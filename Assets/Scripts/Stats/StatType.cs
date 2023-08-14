b64276b3dfb8f4f33d9253f4eb73a9d6cd62246b
Christian <logicismmc@gmail.com>
2023-08-14 16:05:49 +0800

    Base stat systems done.

--- Assets/Scripts/Stats/StatType.cs
+++ Assets/Scripts/Stats/StatType.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class StatType : ScriptableObject 
 {
     [SerializeField] 
-    private readonly string _name; 
+    private string _name; 
 
     public string Name => _name;
 
--- Assets/Scripts/Stats/Stats.cs
+++ Assets/Scripts/Stats/Stats.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 using UnityEngine;
-using System.Xml.Serialization;
+using UnityEditor;
 
 // NOTE (Chris): Take note that the inspector for this script is in CustomStatsEditor.cs
 [CreateAssetMenu(fileName = "Stats", menuName = "Scriptable Objects/Stats")]
@@ -13,12 +14,34 @@ public class Stats : ScriptableObject
     {
         public StatType type;
         public float initialValue;
+
+        public virtual KeyValuePair<StatType, IModifiableValue> CreatePair()
+        {
+            return new KeyValuePair<StatType, IModifiableValue>(
+                type,
+                new ModifiableValue(initialValue));
+        }
     }
 
     [Serializable]
     private class BoundedStatMapEntry : StatMapEntry
     {
+        // TODO (Chris): Have the user be able to choose between readonly 
+        // and not lower bound.
         public float initialLowerBoundValue;
+
+        public override KeyValuePair<StatType, IModifiableValue> CreatePair()
+        {
+            ModifiableValue modifiableValue = new(initialValue);
+            return new KeyValuePair<StatType, IModifiableValue>(type,
+                new BoundedModifiableValue(
+                    new BoundedValue(
+                        new ReadOnlyValueContainer(initialLowerBoundValue),
+                        modifiableValue
+                    ),
+                    modifiableValue
+                ));
+        }
     }
 
     [SerializeReference]
@@ -30,9 +53,47 @@ public class Stats : ScriptableObject
     private Dictionary<StatType, IModifiableValue> _stats = new();
     private Dictionary<StatType, IModifiableValue> _instancedStats = new();
 
+    private readonly Dictionary<string, StatType> _statTypeMap = new();
+
+    private IModifiableValue GetStat(string typeName)
+    {
+        return _stats[_statTypeMap[typeName]];
+    }
+
+    private IModifiableValue GetStat(StatType type)
+    {
+        return _stats[type];
+    }
+
     private void OnEnable()
     {
+        _statTypeMap.Clear();
+        _statInitializerList.ForEach(stat => 
+        {
+            var statPair = stat.CreatePair();
+            if (statPair.Key != null && statPair.Value != null)
+            {
+                _stats.Add(statPair.Key, statPair.Value);
+            }
+            _statTypeMap.Add(statPair.Key.name, statPair.Key);
+        });
 
+        _instancedStatInitializerList.ForEach(stat => 
+        {
+            var statPair = stat.CreatePair();
+            if (statPair.Key != null && statPair.Value != null)
+            {
+                _instancedStats.Add(statPair.Key, statPair.Value);
+            }
+            _statTypeMap.Add(statPair.Key.name, statPair.Key);
+        });
+    }
+
+    // Creates a new instanced dictionary of runtime stats.
+    private Dictionary<string, IModifiableValue> GetInstancedStats()
+    {
+        return _instancedStats.ToDictionary(
+            kv => kv.Key.Name, kv => kv.Value.Clone() as IModifiableValue);
     }
 
     public void CreateBoundedStat()
