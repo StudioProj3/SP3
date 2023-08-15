a5fa17a3a4e781c8981da7c87ab06511897a8c27
Christian <logicismmc@gmail.com>
2023-08-15 19:56:10 +0800

    Created `RequestModify` function.

--- Assets/Scriptable Objects/Scripts/Inventory/Base/InventoryBase.cs
+++ Assets/Scriptable Objects/Scripts/Inventory/Base/InventoryBase.cs
@@ -29,6 +29,80 @@ public abstract class InventoryBase :
     [SerializeField]
     protected List<Pair<ItemBase, uint>> _allItems = new();
 
+    protected Dictionary<int, int> _indexToQuantityMap = new();
+
+    // Function returns whether the modification request is valid. It caches the data
+    // needed for the modification.
+    protected virtual bool RequestModify(ItemBase item, int number)
+    {
+        Assert.IsTrue(number != 0,
+            "`number` is zero");
+
+        bool isAdd = number > 0;
+        int numberLeft = number;
+        bool isItemStackable = item.Stackable;
+
+        // Loop through all the slots available
+        for (int i = 0; i < _maxNumSlots; ++i)
+        {
+            // If all adding or removing was done
+            if ((isAdd && numberLeft <= 0) ||
+                (!isAdd && numberLeft >= 0))
+            {
+                break;
+            }
+
+            ItemBase currentItem = _allItems[i].Key;
+            uint quantity = _allItems[i].Value;
+            bool isNull = currentItem == null;
+            bool isCurrentStackable = currentItem.Stackable;
+
+            // Check if `item` is the not that same as
+            // `currentItem`, if so continue to next iteration
+            if (item != currentItem)
+            {
+                continue;
+            }
+
+            if (isAdd)
+            {
+                // There is already an unstackable item in
+                // this slot, move on to the next iteration
+                if (!isNull && !currentItem.Stackable)
+                {
+                    continue;
+                }
+
+                // If the current slot is already full move
+                // to the next iteration
+                if (quantity >= _maxPerSlot)
+                {
+                    continue;
+                }
+
+                // TODO (Cheng Jun): Continue adding item codes
+
+                // Get the difference that we are able to fill up.
+                int difference = (int)(_maxPerSlot - quantity);
+                
+                // Check if we can fill up the slot fully.
+                if (numberLeft >= difference)
+                {
+                    _indexToQuantityMap.Add(i, difference);
+                    numberLeft -= difference;
+                }
+                
+            }
+            else
+            {
+                // TODO (Cheng Jun): Continue removing item codes
+            }
+        }
+
+        return true;
+        
+    }
+
     // Add or remove items from the inventory based on the sign
     // of `number` with it returning whether the operation was
     // a success
