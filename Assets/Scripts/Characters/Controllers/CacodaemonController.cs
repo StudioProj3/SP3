4a9f46f9aa180eeb05b7c7055f8879af73fd5c1c
Aquila17 <220301B@mymail.nyp.edu.sg>
2023-08-15 14:28:43 +0800

    Updated enemy script.
    - Enemy can now move towards player.
    - Changed tags of objects in surface layer to the correct tags.

--- Assets/Scripts/Characters/Controllers/CacodaemonController.cs
+++ Assets/Scripts/Characters/Controllers/CacodaemonController.cs
@@ -1,14 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 [DisallowMultipleComponent]
 public class CacodaemonController : CharacterControllerBase, IEffectable
 {
-
     [SerializeField]
     private Stats _cacodaemonStats;
 
-    private StatusEffectBase _statusEffect;
+    private GameObject _player;
+
+    private List<StatusEffectBase> _statusEffects = new();
     private float _currentEffectTime;
     private float _nextTickTime;
+
+    private Vector3 _direction;
+
+    IStatContainer IEffectable.EntityStats => _cacodaemonStats;
 
     public void TakeDamage(Damage damage)
     {
@@ -17,33 +23,20 @@ public class CacodaemonController : CharacterControllerBase, IEffectable
 
     public void ApplyEffect(StatusEffectBase statusEffect)
     {
-        _statusEffect = statusEffect;
+        _statusEffects.Add(statusEffect);
+        statusEffect.OnApply(this);
     }
 
-    public void HandleEffect()
+    public void RemoveEffect(StatusEffectBase statusEffect)
     {
-        _currentEffectTime += Time.deltaTime;
-
-        if (_currentEffectTime >= _statusEffect.duration)
-        {
-            RemoveEffect();
-        }
-
-        DamageOverTimeEffect damageOverTimeEffect = _statusEffect as DamageOverTimeEffect;
-        SpeedMultiplierEffect speedMultiplierEffect = _statusEffect as SpeedMultiplierEffect;
-        if (damageOverTimeEffect && _currentEffectTime > _nextTickTime)
-        {
-            _nextTickTime += damageOverTimeEffect.tickSpeed;
-            _cacodaemonStats.GetStat("Health").Subtract(damageOverTimeEffect.dotAmount);
-            Debug.Log("Health = " + _cacodaemonStats.GetStat("Health").Value);
-        }
+        int index = _statusEffects.IndexOf(statusEffect);
+        RemoveEffectImpl(statusEffect, index);
     }
 
-    public void RemoveEffect()
+    private void RemoveEffectImpl(StatusEffectBase statusEffect, int index)
     {
-        _statusEffect = null;
-        _currentEffectTime = 0;
-        _nextTickTime = 0;
+        statusEffect.OnExit(this);
+        _statusEffects.RemoveAt(index);
     }
 
     protected override void Start()
@@ -56,16 +49,88 @@ public class CacodaemonController : CharacterControllerBase, IEffectable
     protected override void SetupStateMachine()
     {
         base.SetupStateMachine();
+
+        _stateMachine.AddChilds
+       (
+           new GenericState("Walk",
+               new ActionEntry("FixedUpdate", () =>
+               {
+                   _rigidbody.velocity = _cacodaemonStats.GetStat("MoveSpeed").Value * _direction.normalized;
+               })
+           ),
+
+           new GenericState("Charge",
+               new ActionEntry("Enter", () =>
+               {
+
+                   //Vector3 direction =
+                       //new(_horizontalInput, 0, _verticalInput);
+
+                   _rigidbody.AddForce(
+                       _cacodaemonStats.GetStat("MoveSpeed").Value *
+                       2 * _direction.normalized,
+                       ForceMode.Impulse
+                       );
+               })
+           ),
+
+           // Transitions
+           // Idle > Walk
+           new FixedTimedTransition("Idle", "Walk", 0.5f),
+
+           // Walk > Idle
+           new FixedTimedTransition("Walk", "Idle", 0.5f),
+
+           // Charge > Idle
+           new FixedTimedTransition("Charge", "Idle", 0.7f)
+
+           //// Walk or Idle > Roll
+           //new EagerGenericTransition("Walk", "Roll", () =>
+           //{
+           //    return _rollKeyDown;
+           //}),
+
+           //new EagerGenericTransition("Idle", "Roll", () =>
+           //{
+           //    return _rollKeyDown;
+           //})
+       );
+
+        _stateMachine.SetStartState("Idle");
+
         _stateMachine.Enter();
+    }
+
+    private void Awake()
+    {
+        _player = GameObject.FindWithTag("Player");
     }
 
     private void Update()
     {
-        
+        _animator.SetBool("isCharging",
+            _stateMachine.CurrentState.StateID == "Charge");
+
+        if (!_statusEffects.IsNullOrEmpty())
+        {
+            _statusEffects.ForEach(effect => effect.HandleEffect(this));
+        }
+
+        for (int i = 0; i < _statusEffects.Count; ++i)
+        {
+            if (_statusEffects[i].IsDone)
+            {
+                RemoveEffectImpl(_statusEffects[i], i);
+                --i;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
+        Debug.Log(_player.transform.position);
+        _direction = _player.transform.position - transform.position;
+        _direction.y = 0;
         _stateMachine.FixedUpdate();
     }
 }
