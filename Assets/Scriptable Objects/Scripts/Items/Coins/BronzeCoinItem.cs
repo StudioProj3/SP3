003e9b7165ed205b565e9dfda3cd651f0e22e64e
Rageking8 <tomleetyt@gmail.com>
2023-08-16 13:57:21 +0800

    Add bronze, silver and gold coin scripts and assets.

---
+++ Assets/Scriptable Objects/Scripts/Items/Coins/BronzeCoinItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BronzeCoin",
+    menuName = "Items/Coins/BronzeCoin")]
+public class BronzeCoinItem : CoinItemBase
+{
+
+}
---
+++ Assets/Scriptable Objects/Scripts/Items/Coins/GoldCoinItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoldCoin",
+    menuName = "Items/Coins/GoldCoin")]
+public class GoldCoinItem : CoinItemBase
+{
+
+}
