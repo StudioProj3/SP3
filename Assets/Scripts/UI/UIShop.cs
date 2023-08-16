f63cc5fd87b14b3b771fb5c92d171eb3ee12099c
Christian <97773844+esuriru@users.noreply.github.com>
2023-08-17 00:44:14 +0800

    Added materials list.

--- Assets/Scripts/UI/UIShop.cs
+++ Assets/Scripts/UI/UIShop.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class UIShop : MonoBehaviour
 {
-    
+    [SerializeField]
+    private List<ShopItem> _materials = new();
+
+    private void Start()
+    {
+
+    }
 }
