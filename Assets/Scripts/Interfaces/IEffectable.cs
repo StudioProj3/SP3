2d0c5652354b2d4d476d1050700d637388ec1227
Rageking8 <tomleetyt@gmail.com>
2023-08-17 20:52:31 +0800

    Remove superfluous `using`s in `IEffectable` interface.

--- Assets/Scripts/Interfaces/IEffectable.cs
+++ Assets/Scripts/Interfaces/IEffectable.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using UnityEngine;
-
 public interface IEffectable : IDamageable
 {
     public IStatContainer EntityStats { get;}
