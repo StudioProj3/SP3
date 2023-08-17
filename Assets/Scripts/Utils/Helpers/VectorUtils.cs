1b64d00821f74818182098c453933e01535434ab
Rageking8 <tomleetyt@gmail.com>
2023-08-18 03:46:18 +0800

    Create `Vector2` `Set` overload in `VectorUtils`.

--- Assets/Scripts/Utils/Helpers/VectorUtils.cs
+++ Assets/Scripts/Utils/Helpers/VectorUtils.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public static class VectorUtils
 {
+    public static Vector2 Set(this Vector2 vector2,
+        float x = float.MaxValue, float y = float.MaxValue)
+    {
+        vector2 = new(x == float.MaxValue ? vector2.x : x,
+            y == float.MaxValue ? vector2.y : y);
+
+        return vector2;
+    }
+
     public static Vector3 Set(this Vector3 vector3,
         float x = float.MaxValue, float y = float.MaxValue,
         float z = float.MaxValue)
