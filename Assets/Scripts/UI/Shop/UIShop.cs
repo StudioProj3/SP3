90f2f1f50e393e5a8bba01ff17c44ae75e2831a0
Christian <97773844+esuriru@users.noreply.github.com>
2023-08-18 02:30:34 +0800

    Removed unused usings.

--- Assets/Scripts/UI/Shop/UIShop.cs
+++ Assets/Scripts/UI/Shop/UIShop.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using TMPro;
-using Unity.VisualScripting;
+
 using UnityEngine;
 
 using static DebugUtils;
