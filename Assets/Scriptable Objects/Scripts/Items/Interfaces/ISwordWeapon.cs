f48be19a07d02a27c7fe7770b79c0b4debddf3db
Rageking8 <tomleetyt@gmail.com>
2023-08-18 01:17:12 +0800

    Remove superfluous `using`s and some formatting in item interfaces.

--- Assets/Scriptable Objects/Scripts/Items/Interfaces/ISwordWeapon.cs
+++ Assets/Scriptable Objects/Scripts/Items/Interfaces/ISwordWeapon.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using UnityEngine;
-
-public interface ISwordWeapon : IBeginUseHandler, IEndUseHandler
+public interface ISwordWeapon :
+    IBeginUseHandler, IEndUseHandler
 {
     string AnimationName { get; }
 }
