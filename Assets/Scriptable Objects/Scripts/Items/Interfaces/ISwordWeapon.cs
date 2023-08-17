e7ad7e5bb4a426c4c9d3c495bc66c410ed525540
Penguin27564 <86288533+Penguin27564@users.noreply.github.com>
2023-08-18 05:15:28 +0800

    Added sprites for more sword types.

--- Assets/Scriptable Objects/Scripts/Items/Interfaces/ISwordWeapon.cs
+++ Assets/Scriptable Objects/Scripts/Items/Interfaces/ISwordWeapon.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using UnityEngine;
 
 public interface ISwordWeapon : IBeginUseHandler, IEndUseHandler
 {
