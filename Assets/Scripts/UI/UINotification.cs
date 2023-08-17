4f82f473d76aa769e4c691d62a06ad8eb8930097
Rageking8 <tomleetyt@gmail.com>
2023-08-18 02:40:47 +0800

    Add notification UI text `RectTransform` reference to `UINotification` and start creating some layout methods.

--- Assets/Scripts/UI/UINotification.cs
+++ Assets/Scripts/UI/UINotification.cs
@@ -7,6 +7,7 @@ public class UINotification : MonoBehaviour
     private Animator _animator;
     private Image _itemIcon;
     private TMP_Text _notificationText;
+    private RectTransform _notifTextTransform;
 
     public void Collect(Sprite sprite, string name,
         uint quantity = 1, bool forceWrap = false)
@@ -24,5 +25,21 @@ public class UINotification : MonoBehaviour
         _itemIcon = transform.GetChild(0).GetComponent<Image>();
         _notificationText = transform.GetChild(1).
             GetComponent<TMP_Text>();
+        _notifTextTransform = _notificationText.
+            GetComponent<RectTransform>();
+    }
+
+    // Change layout to show a sprite icon at the left side
+    // and text align center with the remaining space
+    private void SpriteTextLayout()
+    {
+        
+    }
+
+    // Change layout to show text only which is center aligned
+    // and takes up the full width
+    private void FullTextLayout()
+    {
+
     }
 }
