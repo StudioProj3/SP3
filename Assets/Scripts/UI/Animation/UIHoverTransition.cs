bd8892bfefebeb7c2fa320cb3340522d2b631814
Rageking8 <tomleetyt@gmail.com>
2023-08-19 16:36:00 +0800

    Create `UIHoverTransition` and `UIHUDHealthBar`.

---
+++ Assets/Scripts/UI/Animation/UIHoverTransition.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class UIHoverTransition : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Whether the transition is applied to this gameobject")]
+    private bool _moveSelf = true;
+}
