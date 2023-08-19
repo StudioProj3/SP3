15dbef0f3b2b93ad808bb1693d103b2d5378cccb
Rageking8 <tomleetyt@gmail.com>
2023-08-19 23:10:34 +0800

    Implement health and sanity HUD bar updating based on player stats.

---
+++ Assets/Scripts/UI/HUD/UIHUDStatBar.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHUDStatBar : MonoBehaviour
+{
+    [SerializeField]
+    private Stats _stats;
+
+    [SerializeField]
+    private StatType _statType;
+
+    private RectMask2D _statBarLeft;
+    private RectMask2D _statBarRight;
+
+    private float length;
+
+    private void Update()
+    {
+        IModifiableValue value = _stats.GetStat(_statType);
+        float ratio = value.Value / value.Max;
+
+        float padding = (1f - ratio) * length;
+
+        _statBarLeft.padding = new(padding, 0f, 0f, 0f);
+        _statBarRight.padding = new(0f, 0f, padding, 0f);
+    }
+
+    private void Awake()
+    {
+        _statBarLeft = transform.GetChild(0).
+            GetComponent<RectMask2D>();
+        _statBarRight = transform.GetChild(1).
+            GetComponent<RectMask2D>();
+
+        length = _statBarLeft.rectTransform.sizeDelta.x;
+    }
+}
