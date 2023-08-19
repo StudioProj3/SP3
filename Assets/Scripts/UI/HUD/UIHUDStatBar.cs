04071c66675cc80256448026d4139fe56739f7bb
Rageking8 <tomleetyt@gmail.com>
2023-08-19 23:16:06 +0800

    Fix missing leading underscore in `UIHUDStatBar`.

--- Assets/Scripts/UI/HUD/UIHUDStatBar.cs
+++ Assets/Scripts/UI/HUD/UIHUDStatBar.cs
@@ -12,14 +12,14 @@ public class UIHUDStatBar : MonoBehaviour
     private RectMask2D _statBarLeft;
     private RectMask2D _statBarRight;
 
-    private float length;
+    private float _length;
 
     private void Update()
     {
         IModifiableValue value = _stats.GetStat(_statType);
         float ratio = value.Value / value.Max;
 
-        float padding = (1f - ratio) * length;
+        float padding = (1f - ratio) * _length;
 
         _statBarLeft.padding = new(padding, 0f, 0f, 0f);
         _statBarRight.padding = new(0f, 0f, padding, 0f);
@@ -32,6 +32,6 @@ public class UIHUDStatBar : MonoBehaviour
         _statBarRight = transform.GetChild(1).
             GetComponent<RectMask2D>();
 
-        length = _statBarLeft.rectTransform.sizeDelta.x;
+        _length = _statBarLeft.rectTransform.sizeDelta.x;
     }
 }
