25ce4669f7f2562c3540501c066b6a9dfad6553f
Rageking8 <tomleetyt@gmail.com>
2023-08-19 12:06:32 +0800

    Wrap long lines in `MagicDamage`.

--- Assets/Scripts/Damage/Damage Types/MagicDamage.cs
+++ Assets/Scripts/Damage/Damage Types/MagicDamage.cs
@@ -15,22 +15,27 @@ public class MagicDamage : Damage
         return this;
     }
 
-    // OnApply is in 'TakeDamage()' implemented from IDamageable
-    // damage.OnApply(_playerStats);
+    // `OnApply` is in 'TakeDamage' implemented from `IDamageable`
+    // `damage.OnApply(_playerStats)`
     public override void OnApply(IEffectable effectable)
     {
         var entityStats = effectable.EntityStats;
-        // DR = (1 – (100/(100 + V))); where V equals the value of armour or magic reduction 
-        // and DR is the percentage of damage that is reduced.
+
+        // DR = (1 – (100/(100 + V))); where V equals the value of
+        // armour or magic reduction and DR is the percentage of
+        // damage that is reduced
         if (entityStats.GetStat("MagicResistance").Value > 1)
         {
-            float resistance = (1 - (100 / (100 + entityStats.GetStat("MagicResistance").Value))) * 100;
-            float damage = Mathf.Round(_damage * ((100 - resistance) / 100));
+            float resistance = (1 - (100 / (100 + entityStats.
+                GetStat("MagicResistance").Value))) * 100;
+            float damage = Mathf.Round(_damage *
+                ((100 - resistance) / 100));
             entityStats.GetStat("Health").Subtract(damage);
         }
         else
         {
-            entityStats.GetStat("Health").Subtract(Mathf.Round(_damage));
+            entityStats.GetStat("Health").Subtract(
+                Mathf.Round(_damage));
         }
     }
 }
