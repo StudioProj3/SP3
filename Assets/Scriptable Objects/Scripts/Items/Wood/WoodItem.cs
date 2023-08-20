4dcca0e194d4a9028cad7ea9f0fc42b57cd992be
Rageking8 <tomleetyt@gmail.com>
2023-08-20 11:41:51 +0800

    Implement `IItemDecay` in `WoodItem`.

--- Assets/Scriptable Objects/Scripts/Items/Wood/WoodItem.cs
+++ Assets/Scriptable Objects/Scripts/Items/Wood/WoodItem.cs
@@ -2,13 +2,38 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Wood",
     menuName = "Scriptable Objects/Items/Wood")]
-public class WoodItem : ItemBase
+public class WoodItem :
+    ItemBase, IItemDecay
 {
+    [field: HorizontalDivider]
+    [field: Header("Item Parameters")]
+
     [field: SerializeField]
     public bool Rotten { get; protected set; }
 
     [field: SerializeField]
     public CurrencyCost CurrencyCost { get; protected set; }
+
+    [field: HorizontalDivider]
+    [field: Header("Decay Parameters")]
+
+    [field: SerializeField]
+    public IItemDecay.DecayDuration DurationType { get; protected set; }
+
+    [field: SerializeField]
+    [ShowIf("DurationType", IItemDecay.DecayDuration.Fixed)]
+    public float FixedDuration { get; protected set; }
+
+    [field: SerializeField]
+    [ShowIf("DurationType", IItemDecay.DecayDuration.Random)]
+    public float MinDuration { get; protected set; }
+
+    [field: SerializeField]
+    [ShowIf("DurationType", IItemDecay.DecayDuration.Random)]
+    public float MaxDuration { get; protected set; }
+
+    [field: SerializeField]
+    public ItemBase Target { get; protected set; }
 
     public WoodItem()
     {
