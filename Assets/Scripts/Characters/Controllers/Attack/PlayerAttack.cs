5b12db323c35b0aef26a0ad277781209413be638
Penguin27564 <86288533+Penguin27564@users.noreply.github.com>
2023-08-20 19:05:05 +0800

    Formatted player deal damage to follow convention.

--- Assets/Scripts/Characters/Controllers/Attack/PlayerAttack.cs
+++ Assets/Scripts/Characters/Controllers/Attack/PlayerAttack.cs
@@ -110,7 +110,11 @@ public class PlayerAttack : MonoBehaviour
             Vector3 knockbackForce = 
                 (hitPos - transform.position).normalized *
                 weapon.WeaponStats.GetStat("Knockback").Value;
-            effectable.TakeDamage(weapon.WeaponDamageType.AddModifier(Modifier.Multiply(_playerData.CharacterStats.GetStat("DamageMultipler").Value,3)), knockbackForce);
+            
+            effectable.TakeDamage(weapon.WeaponDamageType.
+                AddModifier(Modifier.Multiply(
+                _playerData.CharacterStats.GetStat("DamageMultipler").Value,3)
+                ), knockbackForce);
 
             if (weapon.WeaponStatusEffect)
             {
