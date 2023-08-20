78dde480150d78c124573cf8e8e89f933ee1f742
Rageking8 <tomleetyt@gmail.com>
2023-08-20 13:43:17 +0800

    Implement the new `Disable` property in `IItemDecay` to `WoodItem`.

--- Assets/Scriptable Objects/Scripts/Items/Wood/WoodItem.cs
+++ Assets/Scriptable Objects/Scripts/Items/Wood/WoodItem.cs
@@ -18,29 +18,49 @@ public class WoodItem :
     [field: Header("Decay Parameters")]
 
     [field: SerializeField]
-    public IItemDecay.DecayDuration DurationType
-        { get; protected set; }
+    public bool Disable { get; protected set; } = false;
 
     [field: SerializeField]
-    [field: ShowIf("DurationType", IItemDecay.DecayDuration.Fixed,
-        true)]
+    [field: ShowIf("Disable", false, true)]
+    public IItemDecay.DecayDuration DurationType
+        { get; protected set; } = IItemDecay.DecayDuration.Fixed;
+
+    [field: SerializeField]
+    [field: ShowIf("_fixedDurationCond", true)]
     public float FixedDuration { get; protected set; }
 
     [field: SerializeField]
-    [field: ShowIf("DurationType", IItemDecay.DecayDuration.Random,
-        true)]
+    [field: ShowIf("_randomDurationCond", true)]
     public float MinDuration { get; protected set; }
 
     [field: SerializeField]
-    [field: ShowIf("DurationType", IItemDecay.DecayDuration.Random,
-        true)]
+    [field: ShowIf("_randomDurationCond", true)]
     public float MaxDuration { get; protected set; }
 
     [field: SerializeField]
+    [field: ShowIf("Disable", false, true)]
     public ItemBase Target { get; protected set; }
+
+    // For use as implementation detail
+    [SerializeField]
+    [HideInInspector]
+    protected bool _fixedDurationCond = true;
+
+    // For use as implementation detail
+    [SerializeField]
+    [HideInInspector]
+    protected bool _randomDurationCond = false;
 
     public WoodItem()
     {
         Name = "Wood";
+    }
+
+    private void OnValidate()
+    {
+        _fixedDurationCond = !Disable &&
+            DurationType == IItemDecay.DecayDuration.Fixed;
+        _randomDurationCond = !Disable &&
+            DurationType == IItemDecay.DecayDuration.Random;
     }
 }
