af8c5afe63eb2a8785f96e3ace33731cc61003a3
Rageking8 <tomleetyt@gmail.com>
2023-08-20 22:15:30 +0800

    Add `BasicRecipe` script.

---
+++ Assets/Scriptable Objects/Scripts/Crafting/Recipes/BasicRecipe.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BasicRecipe",
+    menuName = "Scriptable Objects/Crafting/Recipes/BasicRecipe")]
+public class BasicRecipe : RecipeBase
+{
+    [field: SerializeField]
+    public ItemBase[] Row1 { get; protected set; }
+
+    [field: SerializeField]
+    public ItemBase[] Row2 { get; protected set; }
+}
