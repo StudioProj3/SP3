95c87e281f7ee2ce30b581a6ab1ac53232f08b4c
Rageking8 <tomleetyt@gmail.com>
2023-08-20 16:34:23 +0800

    Create new stick item.

---
+++ Assets/Scriptable Objects/Scripts/Items/Sticks/StickItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Stick",
+    menuName = "Scriptable Objects/Items/Stick")]
+public class StickItem : ItemBase
+{
+    public StickItem()
+    {
+        Name = "Stick";
+    }
+}
