6da15a781862b1efe7dcfabec245240e5c275bdc
Rageking8 <tomleetyt@gmail.com>
2023-08-21 09:52:27 +0800

    Fix `GenTexture` size issue.

--- Assets/Scripts/Utils/Helpers/SpriteUtils.cs
+++ Assets/Scripts/Utils/Helpers/SpriteUtils.cs
@@ -9,7 +9,7 @@ public static class SpriteUtils
 
         Color[] pixels = sprite.texture.GetPixels(
             (int)sprite.textureRect.x, (int)sprite.textureRect.y,
-            (int)sprite.textureRect.width, (int)sprite.textureRect.height);
+            sprite.texture.width, sprite.texture.height);
 
         texture.SetPixels(pixels);
         texture.Apply();
