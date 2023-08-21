5c8b831a080888b824f5c7faeb07a6e22e3001b2
Aquila17 <220301B@mymail.nyp.edu.sg>
2023-08-21 11:00:54 +0800

    Added enemy spawner for level manager.

---
+++ Assets/Scripts/Game/LevelManager/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private List<CharacterControllerBase> _pooledEnemyList;
+
+    public void SpawnEnemy()
+    {
+        int randomNum = Random.Range(0, _pooledEnemyList.Count);
+        _pooledEnemyList[randomNum].gameObject.SetActive(true);
+        _pooledEnemyList[randomNum].transform.position =
+                               transform.position;
+        _pooledEnemyList[randomNum].transform.SetParent(null);
+    }
+
+    private void Awake()
+    {
+        _pooledEnemyList = new List<CharacterControllerBase>();
+        foreach (Transform child in transform)
+        {
+            _pooledEnemyList.Add(child.GetComponent<CharacterControllerBase>());
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Game/LevelManager/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager/LevelManager.cs
@@ -8,14 +8,22 @@ public class LevelManager : SceneLoader
     [SerializeField]
     private SceneList _sceneList;
 
-    // Start is called before the first frame update
-    void Start()
+    private GameObject[] _spawners;
+    private EnemySpawner[] _enemySpawners;
+
+    private void Awake()
     {
-        
+        _spawners = GameObject.FindGameObjectsWithTag("EnemySpawner");
+        _enemySpawners = new EnemySpawner[_spawners.Length];
+        for(int i = 0; i < _spawners.Length; ++i)
+        {
+            _enemySpawners[i] = _spawners[i].GetComponent<EnemySpawner>();
+            _enemySpawners[i].SpawnEnemy();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
