0e5ea0ae4cf0161c1101d95c1d6b7fd489f9a3a9
Rageking8 <tomleetyt@gmail.com>
2023-08-22 02:36:33 +0800

    Implement `UIInventory` class.

--- Assets/Scripts/UI/Inventory/UIInventory.cs
+++ Assets/Scripts/UI/Inventory/UIInventory.cs
@@ -1,6 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIInventory : MonoBehaviour
 {
+    [SerializeField]
+    private CharacterData _character;
 
+    private GameObject _content;
+    private Transform _contentItems;
+    private Image[] _slots;
+    private GameObject _noMainInventory;
+
+    private void Update()
+    {
+        InventoryBase inventory = _character.Inventory;
+
+        _content.SetActive(inventory);
+        _noMainInventory.SetActive(!inventory);
+
+        if (!inventory)
+        {
+            return;
+        }
+
+        for (int i = 0; i < _slots.Length; ++i)
+        {
+            ItemBase item = inventory.GetItem(i);
+
+            if (item)
+            {
+                _slots[i].sprite = item.Sprite;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _content = transform.GetChild(0).gameObject;
+        _contentItems = _content.transform.GetChild(1);
+
+        _slots = new Image[_contentItems.childCount];
+        for (int i = 0; i < _contentItems.childCount; ++i)
+        {
+            _slots[i] = _contentItems.GetChild(i, 0).
+                GetComponent<Image>();
+        }
+
+        _noMainInventory = transform.GetChild(1).gameObject;
+    }
 }
