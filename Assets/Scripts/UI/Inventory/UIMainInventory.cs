b93112e34428e5943da403289a36f897f83664e5
Rageking8 <tomleetyt@gmail.com>
2023-08-22 22:02:21 +0800

    Add back `UIMainInventory` script to the main inventory UI.

--- Assets/Scripts/UI/Inventory/UIMainInventory.cs
+++ Assets/Scripts/UI/Inventory/UIMainInventory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,16 +14,16 @@ public class UIMainInventory : MonoBehaviour
     [SerializeField]
     private GameObject _slotPrefab;
 
-    private GameObject _content;
+    private GameObject _viewport;
     private Transform _contentItems;
-    private List<Image> _slots = new();
     private GameObject _noMainInventory;
+    private List<Pair<Image, TMP_Text>> _slots = new();
 
     private void Update()
     {
         InventoryBase inventory = _character.Inventory;
 
-        _content.SetActive(inventory);
+        _viewport.SetActive(inventory);
         _noMainInventory.SetActive(!inventory);
 
         if (!inventory)
@@ -34,8 +35,10 @@ public class UIMainInventory : MonoBehaviour
 
         for (int i = 0; i < _slots.Count; ++i)
         {
-            GameObject parent = _slots[i].transform.parent.
-                gameObject;
+            Image image = _slots[i].First;
+            TMP_Text text = _slots[i].Second;
+
+            GameObject parent = image.transform.parent.gameObject;
 
             // There are more `_slots` than `MaxNumSlots`
             if (i >= inventory.MaxNumSlots)
@@ -53,22 +56,26 @@ public class UIMainInventory : MonoBehaviour
 
             if (item)
             {
-                _slots[i].sprite = item.Sprite;
-                _slots[i].color = _slots[i].color.
-                    Set(a: _slots[i].sprite ? 1f : 0f);
+                // Update item icon
+                image.sprite = item.Sprite;
+                image.color = image.color.
+                    Set(a: image.sprite ? 1f : 0f);
+
+                // Update item quantity
+                // TODO (Cheng Jun): Complete item
+                // quantity implementation
             }
         }
     }
 
     private void Awake()
     {
-        _content = transform.GetChild(0).gameObject;
-        _contentItems = _content.transform.GetChild(1);
+        _viewport = transform.GetChild(0).gameObject;
+        _contentItems = _viewport.transform.GetChild(1);
 
         for (int i = 0; i < _contentItems.childCount; ++i)
         {
-            _slots.Add(_contentItems.GetChild(i, 0).
-                GetComponent<Image>());
+            AddSlot(_contentItems.GetChild(i));
         }
 
         _noMainInventory = transform.GetChild(1).gameObject;
@@ -84,10 +91,18 @@ public class UIMainInventory : MonoBehaviour
         {
             GameObject newObject =
                 Instantiate(_slotPrefab, _contentItems.transform);
-            Image image = newObject.transform.GetChild(0).
-                GetComponent<Image>();
 
-            _slots.Add(image);
+            AddSlot(newObject.transform);
         }
+    }
+
+    private void AddSlot(Transform slot)
+    {
+        Image image = slot.GetChild(0).
+            GetComponent<Image>();
+        TMP_Text text = slot.GetChild(1).
+            GetComponent<TMP_Text>();
+
+        _slots.Add(new(image, text));
     }
 }
