c3a6fe7de24087fafe2180c210a3bf4a9b674d1e
Penguin27564 <penguin27564@gmail.com>
2023-08-22 10:52:02 +0800

    Finished book open animation for levelling.

--- Assets/Scripts/Characters/Level Up/PlayerLevelUp.cs
+++ Assets/Scripts/Characters/Level Up/PlayerLevelUp.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class PlayerLevelUp : MonoBehaviour
 {
+
     [SerializeField]
     private CharacterData _playerData;
 
     private IModifiableValue _experiencePoints;
     private IModifiableValue _currentLevel;
+
     
     private void Start()
     {
@@ -14,15 +16,16 @@ public class PlayerLevelUp : MonoBehaviour
             GetStat("ExperiencePoints");
         _currentLevel = _playerData.CharacterStats.
             GetStat("Level");
-
         _experiencePoints.Set(0);
         _currentLevel.Set(1);
+        Debug.Log("Level: " + _currentLevel.Value);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            Debug.Log(_experiencePoints.Value);
             GainExperience(5);
         }
     }
@@ -38,6 +41,8 @@ public class PlayerLevelUp : MonoBehaviour
     }
     private void LevelUp()
     {
+        
+        Debug.Log("Level: " + _currentLevel.Value);
         Modifier experienceLimitModifier = 
             Modifier.Plus(100 * _currentLevel.Value, 2);
 
