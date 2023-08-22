567c3ba890c3054229bb93175b85f4e1e90ee014
Rageking8 <tomleetyt@gmail.com>
2023-08-22 18:58:51 +0800

    Add `UIInventory` tag and `UIInventory` script.

---
+++ Assets/Scripts/UI/Inventory/UIInventory.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    private GameObject _content;
+
+    public void ShowInventory()
+    {
+        _content.SetActive(true);
+    }
+
+    public void HideInventory()
+    {
+        _content.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        _content = transform.GetChild(0).gameObject;
+    }
+}
