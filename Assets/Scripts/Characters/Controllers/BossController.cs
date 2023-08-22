c79a134be9b7882fa3245c8a25d12406db2f4b69
Aquila17 <220301B@mymail.nyp.edu.sg>
2023-08-22 11:50:00 +0800

    Updated boss script.

--- Assets/Scripts/Characters/Controllers/BossController.cs
+++ Assets/Scripts/Characters/Controllers/BossController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class BossController :
     EnemyControllerBase, IEffectable
 {
+    [SerializeField]
+    private LayerMask _weaponLayer;
 
     [SerializeField]
     private StatusEffectBase _arrowStatusEffect;
 
-    private GameObject _pooledArrows;
-    private List<ArrowController> _pooledArrowList;
+    private GameObject _pooledMissles;
+    private List<ArrowController> _pooledMissleList;
+
+    private GameObject _pooledBullets;
+    private List<ArrowController> _pooledBulletList;
 
     private StatContainer _bossStatsContainer;
 
@@ -20,16 +25,25 @@ public class BossController :
     private Vector3 _direction;
     private float _distance;
     private PhysicalDamage _phyDamage;
+    private MagicDamage _magicDamage;
 
     protected override void Start()
     {
         base.Start();
-        _pooledArrows = transform.GetChild(0).gameObject;
-        _pooledArrowList = new List<ArrowController>();
+        _pooledMissles = transform.GetChild(0).gameObject;
+        _pooledMissleList = new List<ArrowController>();
 
-        foreach (Transform child in _pooledArrows.transform)
+        _pooledBullets = transform.GetChild(1).gameObject;
+        _pooledBulletList = new List<ArrowController>();
+
+        foreach (Transform child in _pooledMissles.transform)
         {
-            _pooledArrowList.Add(child.GetComponent<ArrowController>());
+            _pooledMissleList.Add(child.GetComponent<ArrowController>());
+        }
+
+        foreach (Transform child in _pooledBullets.transform)
+        {
+            _pooledBulletList.Add(child.GetComponent<ArrowController>());
         }
 
         _bossStatsContainer = Data.CharacterStats.
@@ -38,6 +52,8 @@ public class BossController :
         EntityStats = _bossStatsContainer;
         _phyDamage = PhysicalDamage.Create(_bossStatsContainer.
             GetStat("AttackDamage").Value);
+        _magicDamage = MagicDamage.Create(_bossStatsContainer.
+            GetStat("AbilityPower").Value);
 
         SetupStateMachine();
     }
@@ -60,18 +76,18 @@ public class BossController :
                 })
             ),
 
-            new GenericState("Shoot",
+            new GenericState("ShootMissle",
                 new ActionEntry("Enter", () =>
                 {
-                    for (int i = 0; i < _pooledArrowList.Count; i++)
+                    for (int i = 0; i < _pooledMissleList.Count; i++)
                     {
-                        if (!(_pooledArrowList[i].gameObject.activeSelf))
+                        if (!(_pooledMissleList[i].gameObject.activeSelf))
                         {
-                            _pooledArrowList[i].Init(_direction, _phyDamage,
-                                _arrowStatusEffect, _pooledArrows.transform);
-                            _pooledArrowList[i].transform.position =
+                            _pooledMissleList[i].Init(_direction, _phyDamage,
+                                _arrowStatusEffect, _pooledMissles.transform);
+                            _pooledMissleList[i].transform.position =
                                 transform.position;
-                            _pooledArrowList[i].transform.SetParent(null);
+                            _pooledMissleList[i].transform.SetParent(null);
 
                             break;
                         }
@@ -79,27 +95,92 @@ public class BossController :
                 })
             ),
 
-            new GenericState("Roll",
+            new GenericState("ShootRapid",
                 new ActionEntry("Enter", () =>
                 {
-                    _direction = transform.position -
-                        _player.transform.position;
-                    _direction.y = 0;
 
-                    _rigidbody.AddForce(_bossStatsContainer.
-                        GetStat("MoveSpeed").Value * 4f * _direction.normalized,
-                        ForceMode.Impulse);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        _ = Delay.Execute(() =>
+                        {
+                            ShootBullet();
+                        }, i * 0.2f);
+                    }
                 })
             ),
 
-            new GenericState("GoingToShoot",
+            new GenericState("ShootLaser",
                 new ActionEntry("Enter", () =>
                 {
-                    _direction = _player.transform.position -
-                        transform.position;
-                    _direction.y = 0;
+                    for (int i = 0; i < _pooledMissleList.Count; i++)
+                    {
+                        if (!(_pooledMissleList[i].gameObject.activeSelf))
+                        {
+                            _pooledMissleList[i].Init(_direction, _phyDamage,
+                                _arrowStatusEffect, _pooledMissles.transform);
+                            _pooledMissleList[i].transform.position =
+                                transform.position;
+                            _pooledMissleList[i].transform.SetParent(null);
+
+                            break;
+                        }
+                    }
                 })
             ),
+
+            new GenericState("Shield",
+                new ActionEntry("FixedUpdate", () =>
+                {
+                    Collider[] weaponTarget;
+                    weaponTarget = Physics.OverlapSphere(transform.position,
+                        0.5f, _weaponLayer, 0);
+
+
+                    for (int i = 0; i < weaponTarget.Length; i++)
+                    {
+                        Vector3 knockbackForce =
+                            (_player.transform.position - transform.position).
+                            normalized * _bossStatsContainer.
+                            GetStat("Knockback").Value;
+
+                        if (!weaponTarget[i].CompareTag("MeleeWeapon"))
+                        {
+                            _playerController.TakeDamage(
+                                _phyDamage, knockbackForce);
+                        }
+                        else if (!weaponTarget[i].CompareTag("RangedWeapon"))
+                        {
+                            weaponTarget[i].gameObject.SetActive(false);
+                            ShootBullet();
+                        }
+                    }
+                })
+            ),
+
+            new GenericState("MeleeDash",
+                new ActionEntry("Enter", () =>
+                {
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        _ = Delay.Execute(() =>
+                        {
+                            _direction = _player.transform.position -
+                                transform.position;
+                            _direction.y = 0;
+
+                            _rigidbody.AddForce(_bossStatsContainer.
+                                GetStat("MoveSpeed").Value * 6f * _direction.normalized,
+                                ForceMode.Impulse);
+                        }, i);
+                    }
+                    
+                })
+            ),
+
+            new GenericState("MeleeDashStart"),
+
+            new GenericState("MeleeDashEnd"),
 
             new GenericState("Cooldown"),
 
@@ -114,36 +195,42 @@ public class BossController :
             }),
 
             // Idle > Walk
-            new RandomTimedTransition("Idle", "Walk", 1.0f, 2.0f),
+            //new RandomTimedTransition("Idle", "Walk", 1.0f, 2.0f),
+
+            new FixedTimedTransition("Idle", "ShootRapid", 0.7f),
+
+              new FixedTimedTransition("ShootRapid", "Idle", 0.7f),
 
             // Walk > Idle
             new FixedTimedTransition("Walk", "Idle", 0.7f),
 
             // Idle > Roll
-            new GenericTransition("Idle", "Roll", () =>
+            new GenericTransition("Idle", "MeleeDashStart", () =>
             {
                 return _distance < 1.0f;
             }),
 
+            // Walk > Idle
+            new FixedTimedTransition("MeleeDashStart", "MeleeDash", 0.5f),
+
+            // Walk > Idle
+            new FixedTimedTransition("MeleeDash", "MeleeDashEnd", 3.0f),
+
+            // Walk > Idle
+            new FixedTimedTransition("MeleeDashEnd", "Idle", 0.5f),
+
             // Idle > Going to shoot
-            new GenericTransition("Idle", "GoingToShoot", () =>
+            new GenericTransition("Walk", "ShootMissle", () =>
             {
                 return _distance < 3.0f;
             }),
 
             // Roll > Going to Shoot
-            new FixedTimedTransition("Roll", "GoingToShoot", 0.5f),
+            new FixedTimedTransition("Shield", "ShootMissle", 0.5f),
 
-            // FIXME (Aquila): Sometimes when boss shoots,
-            // it plays the shoot animation but does not
-            // shoot an arrow. Likely due to transition
-            // timings between states
-
-            //  Going to Shoot > Shoot
-            new FixedTimedTransition("GoingToShoot", "Shoot", 0.6f),
 
             // Shoot > Cooldown
-            new FixedTimedTransition("Shoot", "Cooldown", 0.2f),
+            new FixedTimedTransition("ShootMissle", "Cooldown", 0.2f),
 
             // Cooldown > Idle
             new FixedTimedTransition("Cooldown", "Idle", 0.2f)
@@ -156,18 +243,29 @@ public class BossController :
 
     private void Awake()
     {
+        
         _player = GameObject.FindWithTag("Player");
         _playerController = _player.GetComponent<PlayerController>();
     }
 
     private void Update()
     {
-        _animator.SetBool("isShooting",
-            _stateMachine.CurrentState.StateID == "GoingToShoot");
         _animator.SetBool("isMoving",
             _stateMachine.CurrentState.StateID == "Walk");
-        _animator.SetBool("isRolling",
-            _stateMachine.CurrentState.StateID == "Roll");
+        _animator.SetBool("isMissle",
+            _stateMachine.CurrentState.StateID == "ShootMissle");
+        _animator.SetBool("isRapid",
+            _stateMachine.CurrentState.StateID == "ShootRapid");
+        _animator.SetBool("isLaser",
+            _stateMachine.CurrentState.StateID == "ShootLaser");
+        _animator.SetBool("isMeleeStarting",
+            _stateMachine.CurrentState.StateID == "MeleeDashStart");
+        _animator.SetBool("isMeleeDashing",
+            _stateMachine.CurrentState.StateID == "MeleeDash");
+        _animator.SetBool("isMeleeEnding",
+            _stateMachine.CurrentState.StateID == "MeleeDashEnd");
+        _animator.SetBool("isShield",
+            _stateMachine.CurrentState.StateID == "Shield");
         _animator.SetBool("isDead",
             _stateMachine.CurrentState.StateID == "Death");
 
@@ -206,6 +304,26 @@ public class BossController :
                 (col.transform.position - transform.position).normalized *
                 _bossStatsContainer.GetStat("Knockback").Value;
             _playerController.TakeDamage(_phyDamage, knockbackForce);
+        }
+    }
+
+    private void ShootBullet()
+    {
+        _direction = _player.transform.position -
+                      transform.position;
+        _direction.y = 0;
+        for (int j = 0; j < _pooledBulletList.Count; j++)
+        {
+            if (!(_pooledBulletList[j]
+                .gameObject.activeSelf))
+            {
+                _pooledBulletList[j].Init(_direction, _phyDamage,
+                    _arrowStatusEffect, _pooledMissles.transform);
+                _pooledBulletList[j].transform.position = transform.position;
+                _pooledBulletList[j].transform.SetParent(null);
+
+                break;
+            }
         }
     }
 }
