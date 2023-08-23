5a7ff237043c4a8c822eff03f209cc824ae148f8
Christian <logicismmc@gmail.com>
2023-08-23 16:20:29 +0800

    Added black bar animation.

--- Assets/Scripts/UI/Manager/UIManager.cs
+++ Assets/Scripts/UI/Manager/UIManager.cs
@@ -1,3 +1,23 @@
+using Codice.Client.BaseCommands;
+using UnityEngine;
+
 public class UIManager : Singleton<UIManager>
 {
+    [HorizontalDivider]
+    [Header("Black Bars Static Data")]
+    [SerializeField] 
+    private float _blackBarSpeed = 1.0f;
+
+    private Animator _animator;
+
+    protected override void OnStart()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("displacementSpeed", _blackBarSpeed);
+    }
+
+    public void ShowBars(bool showBars)
+    {
+        _animator.SetBool("showBars", showBars);
+    }
 }
