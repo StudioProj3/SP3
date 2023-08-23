ff84f042d3d1c1603fbee9215dc56c1c25f31584
Rageking8 <tomleetyt@gmail.com>
2023-08-24 00:17:52 +0800

    Create some static methods in `UI` static class for text animations.

--- Assets/Scripts/UI/Utils/UI.cs
+++ Assets/Scripts/UI/Utils/UI.cs
@@ -1,5 +1,32 @@
+using static DebugUtils;
+
 // Bunch of shared UI functionality
 public static class UI
 {
-    
+    public enum TextTag
+    {
+        Voffset,
+    }
+
+    public static string ApplyTag(string text, TextTag tag,
+        int charIndex, int length = 1, string args = "")
+    {
+        Assert(charIndex < text.Length,
+            "`charIndex` is invalid");
+
+        // Insert closing tag
+        text = text.Insert(charIndex + length, ToTagString(tag));
+
+        // Insert opening tag
+        text = text.Insert(charIndex, ToTagString(tag, args, false));
+
+        return text;
+    }
+
+    private static string ToTagString(TextTag tag,
+        string args = "", bool closing = true)
+    {
+        return (closing ? "</" : "<") + tag.ToLower() +
+            (args != "" ? "=" + args : "") + ">";
+    }
 }
