eb79f05ee78a2b7034b5b2ec5c8409b4b1eda186
Christian <logicismmc@gmail.com>
2023-08-24 16:37:39 +0800

    Added `ValueChanged` event to `IModifiableValue`.

--- Assets/Scripts/Stats/Interfaces/IModifiableValue.cs
+++ Assets/Scripts/Stats/Interfaces/IModifiableValue.cs
@@ -1,7 +1,11 @@
+using System;
+
 public interface IModifiableValue : IValue
 {
     float Max { get; }
     float Base { get; }
+
+    event Action ValueChanged;
 
     void AddModifier(Modifier modifier);
     void RemoveModifier(Modifier modifier);
