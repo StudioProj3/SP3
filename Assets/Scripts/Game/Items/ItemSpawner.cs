26a52f5e26d487a0e94a36e3a65a737ca7c9ae09
Christian <97773844+esuriru@users.noreply.github.com>
2023-08-24 22:26:23 +0800

    Created `ItemSpawner` script.

---
+++ Assets/Scripts/Game/Items/ItemSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Collections;
+
+using UnityEngine.Pool;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    // [SerializeField]
+    private void Awake()
+    {
+        // Spawn all the starting objects
+    }
+}
