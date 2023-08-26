bff416608669002f469cd68bbaa5a1dd2aa1e407
Rageking8 <tomleetyt@gmail.com>
2023-08-26 13:26:01 +0800

    Attempt to fix item slot referencing issues.

--- Assets/Scripts/UI/Item/UIItemSlot.cs
+++ Assets/Scripts/UI/Item/UIItemSlot.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 
 public class UIItemSlot : MonoBehaviour
 {
-    private Image _itemIcon;
     private TMP_Text _itemQuantity;
 
     public void SetIcon(Sprite sprite)
     {
-        _itemIcon.sprite = sprite;
-        _itemIcon.color = _itemIcon.color.
+        Image icon = transform.GetChild(0).
+            GetComponent<Image>();
+        icon.sprite = sprite;
+        icon.color = icon.color.
             Set(a: sprite ? 1f : 0f);
     }
 
@@ -27,8 +28,6 @@ public class UIItemSlot : MonoBehaviour
 
     protected virtual void Awake()
     {
-        _itemIcon = transform.GetChild(0).
-            GetComponent<Image>();
         _itemQuantity = transform.GetChild(1).
             GetComponent<TMP_Text>();
     }
