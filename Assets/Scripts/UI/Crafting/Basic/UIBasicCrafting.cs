2d0f41f2d6204aff2d847171422346f956ff6659
Rageking8 <tomleetyt@gmail.com>
2023-08-27 21:07:00 +0800

    Add `UIBasicCrafting` and implement toggling of crafting UI modes.

---
+++ Assets/Scripts/UI/Crafting/Basic/UIBasicCrafting.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+using static DebugUtils;
+
+public class UIBasicCrafting : MonoBehaviour
+{
+    private UICrafting _uicrafting;
+    private GameObject _craftButton;
+    private GameObject _book;
+
+    private void Update()
+    {
+        switch (_uicrafting.CurrentMode)
+        {
+            case UICrafting.Mode.Craft:
+                _craftButton.SetActive(true);
+                _book.SetActive(false);
+                break;
+
+            case UICrafting.Mode.Book:
+                _book.SetActive(true);
+                _craftButton.SetActive(false);
+                break;
+
+            default:
+                Fatal("Unhandled mode");
+                break;
+        }
+    }
+
+    private void Awake()
+    {
+        _uicrafting = GameObject.FindWithTag("UICrafting").
+            GetComponent<UICrafting>();
+        _craftButton = transform.ChildGO(2);
+        _book = transform.ChildGO(3);
+    }
+}
