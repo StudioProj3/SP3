50368335f3ca3a827035286beeddf932979f93be
Rageking8 <tomleetyt@gmail.com>
2023-08-28 00:57:36 +0800

    Fix player infinite death.

--- Assets/Scripts/UI/PlayerRespawn/UIPlayerRespawn.cs
+++ Assets/Scripts/UI/PlayerRespawn/UIPlayerRespawn.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class UIPlayerRespawn : MonoBehaviour
 {
+    [SerializeField]
+    public CharacterData _playerData; 
+
     public static event Action BeginPlayerRespawn;
     public void RespawnPlayer()
     {
+        _playerData.CharacterStats.GetStat("Health").Set(_playerData.CharacterStats.GetStat("Health").Max);
         BeginPlayerRespawn();
     }
 }
